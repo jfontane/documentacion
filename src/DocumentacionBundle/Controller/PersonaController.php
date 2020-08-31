b1f14502dcda4ed7f2095754bf9fe17d92ab81db
jfontanellaz <jfontanellaz@gmail.com>
2020-08-31 12:53:59 -0300

    Se agrego PersonaController

---
+++ src/DocumentacionBundle/Controller/PersonaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DocumentacionBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PersonaController extends Controller
+{
+    public function indexAction()
+    {
+
+      $em = $this->getDoctrine()->getManager();
+      $representados = $em->getRepository('DocumentacionBundle:Persona')->findBy(array('cuil'=>'20249128342'));
+      dump($representados);die;
+      return $this->render('@Documentacion\Default\index.html.twig');
+    }
+}
